930e4a22752e8a27e3e92e10649dcc70febc7fa7
stephbegle <stephbegle@gmail.com>
2021-02-13 22:01:22 -0800

    first commit to save work

--- code-challenges.js
+++ code-challenges.js
@@ -9,67 +9,212 @@
 // --------------------1) Create a function that determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected outcome: "85 is below boiling point" "350 is above boiling point" "212 is at boiling point"
 
-var temp1 = 85
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 85
+// var temp2 = 350
+// var temp3 = 212
+
+// const tempCheck = (num) => {
+//     let boil = 212
+//     if(num < boil){
+//         return `${num} is below boiling point.`
+//     } else if(num > boil){
+//         return `${num} is above boiling point.`
+//     } else if(num === boil) {
+//         return `${num} is at boiling point.`
+//     } else {
+//         return "You are out of range"
+//     }
+// }
+// console.log(tempCheck(212))
+
+
+
+// // --------------------2) Create a function that multiplies each number in the array by 5 using a for loop.
+// // Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45] [10, 25, -40, 100, 160]
+
+// var myForNumbers1 = [3, 7, 0, 6, -9]
+// var myForNumbers2 = [2, 5, -8, 20, 32]
+
+// const multiply = (array) => {
+//     let newArr = []
+//     for(i = 0; i < array.length; i++){
+//         newArr.push(array[i] * 5)
+//     }
+//     return newArr
+// } 
+// console.log(multiply(myForNumbers1))
+// console.log(multiply(myForNumbers2))
+
+
+
+// // --------------------3) Create a function that multiplies each number in the array by 5 using map.
+// // Use the test variable provided below. Expected outcome: [435, 30, 140, 80, 10] [40, -35, 0, 30, 60]
+
+// var myMapNumbers1 = [87, 6, 28, 16, 2]
+// var myMapNumbers2 = [8, -7, 0, 6, 12]
+
+// const mapMult = (array) => {
+//     return array.map(value => {
+//         return value * 5
+//     })
+// }
+
+// console.log(mapMult(myMapNumbers1))
+// console.log(mapMult(myMapNumbers2))
+
+
+
+// // --------------------4) Create a function that removes all the vowels from a string.
+// // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
+
+// var stringWithVowels1 = "HeyThereLearnStudent"
+// var stringWithVowels2 = "ILoveJavaScript"
+
+// var str = "Thisisastring"
+
+// const onlyVowel = (string) => {
+//     var spStr = string.split("")
+//     var result = []
+//     for(i = 0; i < spStr.length; i++){
+//         if(spStr[i] === "a" || spStr[i] === "e" || spStr[i] === "i" || spStr[i] === "o" || spStr[i] === "u"){ 
+//             continue; // this continue statement allows the program to ignore the vowels and continue iterating through the string and with the else statment below, only add the consonants.
+//         } else {
+//             result.push(spStr[i])
+//         }
+//     }
+//     return result.join("")
+// }
+// console.log(onlyVowel(stringWithVowels1))
+// console.log(onlyVowel(stringWithVowels2))
+
+
+
+
+// // --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user that the variable passed into the vowel removal function is not a string.
+// // Use the test variables provided below. Expected output: "true is not a string" "42 is not a string" "mCdngMstr"
+
+// var refactorTester1 = true
+// var refactorTester2 = 42
+// var refactorTester3 = "IAmACodingMaster"
+
+// const onlyVowel = (string) => {
+//     if(typeof string !== "string"){
+//         return `${string} is not a string`
+//     }
+//     var spStr = string.split("")
+//     var result = []
+//     for(i = 0; i < spStr.length; i++){
+//         if(spStr[i] === "a" || spStr[i] === "e" || spStr[i] === "i" || spStr[i] === "o" || spStr[i] === "u"){ 
+//             continue; // this continue statement allows the program to ignore the vowels and continue iterating through the string and with the else statment below, only add the consonants.
+//         } else {
+//             result.push(spStr[i])
+//         }
+//     }
+//     return result.join("")
+// }
+// console.log(onlyVowel(true))
+// console.log(onlyVowel(42))
+// console.log(onlyVowel("IAmACodingMaster"))
 
 
 
 
 
-// --------------------2) Create a function that multiplies each number in the array by 5 using a for loop.
-// Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45] [10, 25, -40, 100, 160]
-
-var myForNumbers1 = [3, 7, 0, 6, -9]
-var myForNumbers2 = [2, 5, -8, 20, 32]
-
-
-
-
-
-// --------------------3) Create a function that multiplies each number in the array by 5 using map.
-// Use the test variable provided below. Expected outcome: [435, 30, 140, 80, 10] [40, -35, 0, 30, 60]
-
-var myMapNumbers1 = [87, 6, 28, 16, 2]
-var myMapNumbers2 = [8, -7, 0, 6, 12]
-
-
-
-
-
-// --------------------4) Create a function that removes all the vowels from a string.
-// Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
-
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
-
-
-
-
-
-// --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user that the variable passed into the vowel removal function is not a string.
-// Use the test variables provided below. Expected output: "true is not a string" "42 is not a string" "mCdngMstr"
-
-var refactorTester1 = true
-var refactorTester2 = 42
-var refactorTester3 = "IAmACodingMaster"
-
-
-
-
-
-// --------------------6) Create a function that takes in an array of strings and returns the string with the most characters.
-// Expected output: "chopsticks" "stemware"
+// // --------------------6) Create a function that takes in an array of strings and returns the string with the most characters.
+// // Expected output: "chopsticks" "stemware"
 
 var utensils = ["fork", "knife", "tongs", "chopsticks", "skewer"]
 var vessels = ["cup", "mug", "stemware", "glass", "tumbler", "tea cup"]
 
+    // const alphaString = (array) => { // trying a train of thought
+    //     return array.filter(value, index => {
+    //         let counter = 0
+    //         let ind;
+    //         if(value.length <= counter){
+    //             continue;
+    //         } else if(value.length >= counter){
+    //             counter = value.length
+    //             ind = index
+    //         }
+    //         return array[ind]
+    //     })
+    // }
+    // console.log(alphaString(utensils))
+// this shit needs work **************
+
+
+// const alphaString = (array) => { // trying a different train of thought
+//     let counter = 0;
+//     let lenArr = [];
+//     for(i = 0; i < array.length; i++){
+//         lenArr.push(array[i].length);
+//         for(i = 0; i < lenArr.length; i++){
+//             if(lenArr[i] >= counter){
+//                 counter = lenArr[i]
+//             }
+//         }
+//     }
+//     let ind = lenArr.indexOf(counter)
+//     return array[ind]
+// }
+// console.log(alphaString(vessels))
+
+var nums = [4, 5, 5, 30, 6, 1] // tryna understand how this shit works 
+var counter = 0
+for(i = 0; i < nums.length; i++){
+    if(nums[i] >= 0){
+        counter = nums[i]
+    }
+}
+console.log(counter)
+
+// const alphaString = (array) => { // trying a different train of thought
+//     let counter = 0;
+//     let ind = 0;
+//     for(i = 0; i < array.length; i++){
+//         let len = array[i].length;
+//         if(len > counter){
+//             ind = i
+//         }
+//     }
+//     return array[ind]
+// }
+// console.log(alphaString(vessels))
+
+// loop through the array
+// count the length of every element
+// add those lengths to another array 
+// find the largest number and its index 
 
 
 
+// // --------------------7) Create a function that takes in an array of strings and returns an array with the last letter removed from each string.
+// // Expected output: ["Luig", "Peac", "Toa", "Yosh"] ["Ink", "Blink", "Pink", "Clyd"]
 
-// --------------------7) Create a function that takes in an array of strings and returns an array with the last letter removed from each string.
-// Expected output: ["Luig", "Peac", "Toa", "Yosh"] ["Ink", "Blink", "Pink", "Clyd"]
+// var mario = ["Luigi", "Peach", "Toad", "Yoshi"]
+// var pacman = ["Inky", "Blinky", "Pinky", "Clyde"]
 
-var mario = ["Luigi", "Peach", "Toad", "Yoshi"]
-var pacman = ["Inky", "Blinky", "Pinky", "Clyde"]
+// // higher order function 
+// var circumSize = (array) => { // this function returns the inside portion also the name was used for comedic (dark) reasons and will be changed before actual submission lol 
+//     return array.map(value => {
+//         let split = value.split("");
+//         split.pop();
+//         return split.join("") // this return is what adds every newly chopped-off word into a new array
+//     })
+// }
+// console.log(circumSize(mario))
+// console.log(circumSize(pacman))
+
+// // regular function 
+// const circum = (array) => { // dark comedic purposes on the func. name, will change before submission
+//     let newArr = []
+//     for(i = 0; i < array.length; i++){
+//         let split = array[i].split("");
+//         split.pop();
+//         let joined = split.join("") // had to set to a variable for .join("") to actually do its job
+//         newArr.push(joined);    
+//     }
+//     return newArr
+// }
+// console.log(circum(mario))
+// console.log(circum(pacman))
--- interview-questions.md
+++ interview-questions.md
@@ -7,7 +7,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: A function is a block of code that performs a specific duty. It can take in no arguments, or take in one or more arguments. You would use a function in a situation where you are performing a repetative task that could require alot of code. Things would be tedious and messy if we had to constantly perform a specific task over and over again; mistakes are high when we do that. So to save time, hardship and acheive conciseness, functions are tools that help writing programs become easier, faster, and neater. 
 
   Researched answer:
 
@@ -15,7 +15,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 2. What is the difference between .map() and .filter()?
 
-  Your answer:
+  Your answer: .map() is a method that takes in a function loops through its argument and returns the same type and length of an argument it is fed. .filter() does the same, except, it returns the product of a specified conditional within the function. The length could be larger or smaller than what the argument it is fed.   
 
   Researched answer:
 
@@ -23,7 +23,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 3. What is the difference between console.log() and return?
 
-  Your answer:
+  Your answer: console.log() is like a print statement, where it just logs whatever it is fed in the console. It is helpful for debugging but also to visualize what your program is running. 
 
   Researched answer:
 
@@ -31,7 +31,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 4. In regards to functions, what is an argument?
 
-  Your answer:
+  Your answer: An argument is a statement or requirement that is fed into the function. For example: if the function you are looking at is meant to observe and do something to arrays, then its argument should be an array, and the same goes with any other data type. Some functions don't require arguments, but most require either one or more arguments to run. 
 
   Researched answer:
 
@@ -39,7 +39,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: Assuming we are working with the drive/navigate technique of pair programming, some techniques that set both parties up for a smooth experience/success are communicating honestly, using accurate vocabulary, understanding that people have different ways of looking at and processing information, assuming good intent in your partners' words and behaviors, and maybe adressing an issue politely if there feels to be one in the moment. If one person is the driver, then they should be listening to their partner (the navigator), following their input. That's not to say the driver has no input and is supposed to soley be the navigator's fingers, but they should allow the navigator to dictate what is to be done.   
 
   Researched answer:
 
@@ -47,7 +47,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 6. What is iteration?
 
-  Your answer:
+  Your answer: An iteration is one run-through of a line or block of code. If you have one line of code and run that, that is one iteration. If you run that same line again, that is another iteration. Loops, for example, go through however many "iterations" you assign it to run. If the second argument in a for-loop is specified to 5, then that loop will iterate through your code 5 times.    
 
   Researched answer:
 
@@ -55,7 +55,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 7. What are advantages and disadvantages for using terminal commands to interact with your computer?
 
-  Your answer:
+  Your answer: Advantages for using the terminal to interact with your computer are that it can be quicker access to certain files and it's neater because you don't have to open a bunch of GUI windows. You can also run and see the results of your program within the terminal without opening the text editor in which your program lives. Some disadvantages are that you can get a little lost in the navigation of certain files, accidental deletions of important files can happen, the changing of certain locations of files can happen, etc. It is a fragile tool that can be deadly if used improperly, but seamless if used sagely. 
 
   Researched answer:
 
@@ -63,7 +63,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 8. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: Definitely learning git has been helpful. I used to be terrified of what the terminal was capable of doing since I knew and understood very little about how it functioned. Now I feel much more confident transitioning from file to file, along with pushing and pulling files onto github. 
 
 
 
